6085cb7be7fc6027fa6355fd3d40443a9084c8b6
Praytic <chernogorovvm@gmail.com>
2025-08-27 21:41:58 -0700

    Fix compilation errors in example

--- examples/index.ts
+++ examples/index.ts
@@ -1,15 +1,12 @@
-import * as pulumi from "@pulumi/pulumi";
-import { SchemaVersion, FileMigrationManager } from "postgresql-schema-migration-pulumi";
+import { FileMigrationManager } from "postgresql-schema-migration-pulumi";
 import * as path from "path";
 
-const config = new pulumi.Config("postgresql");
 const migrations = new FileMigrationManager(path.join(__dirname, "sql"));
 const migrationResources = migrations.createMigrationResources();
 
 export const migrationCount = migrationResources.length;
 export const appliedMigrations = migrationResources.map(migration => ({
-    name: migration.name,
-    version: migration.version
+    urn: migration.urn,
 }));
 const lastMigration = migrations.getMigrationFiles().slice(-1)[0];
 export const currentMigrationVersion = lastMigration ? lastMigration.version : null;
